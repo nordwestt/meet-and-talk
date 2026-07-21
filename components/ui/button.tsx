b91d10bf676eed2c62253820b3966d12d8f5ae4e
Thomas Nordentoft <thomas@nordentoft.dk>
2026-07-21 15:41:06 +0200

    Fix small dom error

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import { Button as ButtonPrimitive } from '@base-ui/react/button'
 import { cva, type VariantProps } from 'class-variance-authority'
 
@@ -44,14 +45,28 @@ function Button({
   className,
   variant = 'default',
   size = 'default',
+  asChild = false,
+  render,
+  children,
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: ButtonPrimitive.Props &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const childRender =
+    render ??
+    (asChild && React.isValidElement(children) ? children : undefined)
+
   return (
     <ButtonPrimitive
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      render={childRender}
+      nativeButton={!childRender}
       {...props}
-    />
+    >
+      {childRender ? undefined : children}
+    </ButtonPrimitive>
   )
 }
 
